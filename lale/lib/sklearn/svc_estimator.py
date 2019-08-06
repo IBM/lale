f022590048e5df2e36f9534083d2063821bc283d
kakate@us.ibm.com <kakate@us.ibm.com>
2019-08-06 12:51:07 -0400

    First commit in the new repository location.

---
+++ lale/lib/sklearn/svc_estimator.py
@@ -0,0 +1,243 @@
+# Copyright 2019 IBM Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import sklearn.svm.classes
+import lale.helpers
+import lale.operators
+
+
+class SVCImpl():
+
+    def __init__(self, C=1.0, kernel='rbf', degree=3, gamma='auto', coef0=0.0, shrinking=True, probability=False, tol=0.001, cache_size=200, class_weight=None, verbose=False, max_iter=-1, decision_function_shape='ovr', random_state=None):
+        self._hyperparams = {
+            'C': C,
+            'kernel': kernel,
+            'degree': degree,
+            'gamma': gamma,
+            'coef0': coef0,
+            'shrinking': shrinking,
+            'probability': probability,
+            'tol': tol,
+            'cache_size': cache_size,
+            'class_weight': class_weight,
+            'verbose': verbose,
+            'max_iter': max_iter,
+            'decision_function_shape': decision_function_shape,
+            'random_state': random_state}
+
+    def fit(self, X, y=None, sample_weight=None):
+        self._sklearn_model = sklearn.svm.classes.SVC(**self._hyperparams)
+        self._sklearn_model.fit(X, y, sample_weight)
+        return self
+
+    def predict(self, X):
+        return self._sklearn_model.predict(X)
+
+    def predict_proba(self, X):
+        return self._sklearn_model.predict_proba(X)
+
+
+_hyperparams_schema = {
+    '$schema': 'http://json-schema.org/draft-04/schema#',
+    'description': 'C-Support Vector Classification.',
+    'allOf': [{
+        'type': 'object',
+        'additionalProperties': False,
+        'required': ['kernel', 'degree', 'gamma', 'shrinking', 'tol', 'cache_size', 'max_iter', 'decision_function_shape'],
+        'relevantToOptimizer': ['kernel', 'degree', 'gamma', 'shrinking', 'probability', 'tol'],
+        'properties': {
+            'C': {
+                'type': 'number',
+                'description': 'Penalty parameter C of the error term.',
+                'distribution': 'loguniform',
+                'default': 1.0,
+                'minimumForOptimizer': 0.03125,
+                'maximumForOptimizer': 32768},
+            'kernel': {
+                'anyOf': [
+                    {'enum':['precomputed'], 'forOptimizer': False}, 
+                    {'enum': ['linear', 'poly', 'rbf', 'sigmoid']}],
+                    #support for callable is missing as of now.               
+                'default': 'rbf',
+                'description': 'Specifies the kernel type to be used in the algorithm.'},
+            'degree': {
+                'type': 'integer',
+                'minimumForOptimizer': 2,
+                'maximumForOptimizer': 5,
+                'default': 3,
+                'description': "Degree of the polynomial kernel function ('poly')."},
+            'gamma': {
+                'anyOf': [{
+                    'type': 'number',
+                    'minimumForOptimizer': 3.0517578125e-05,
+                    'maximumForOptimizer': 8,
+                    'distribution': 'loguniform'},
+                    {'enum': ['auto', 'auto_deprecated', 'scale']}
+                    ],
+                'default': 'auto_deprecated', #going to change to 'scale' from sklearn 0.22.
+                'description': "Kernel coefficient for 'rbf', 'poly' and 'sigmoid'."},
+            'coef0': {
+                'type': 'number',
+                'default': 0.0,
+                'description': 'Independent term in kernel function.'},
+            'shrinking': {
+                'type': 'boolean',
+                'default': True,
+                'description': 'Whether to use the shrinking heuristic.'},
+            'probability': {
+                'type': 'boolean',
+                'default': False,
+                'description': 'Whether to enable probability estimates.'},
+            'tol': {
+                'type': 'number',
+                'distribution':'loguniform',
+                'minimumForOptimizer': 1e-08,
+                'maximumForOptimizer': 0.01,
+                'default': 0.001,
+                'description': 'Tolerance for stopping criterion.'},
+            'cache_size': {
+                'type': 'integer',
+                'default': 200,
+                'description': 'Specify the size of the kernel cache (in MB).'},
+            'class_weight': {
+                'anyOf': [{
+                    'type': 'object'}, {
+                    'enum': ['balanced', None]}],
+                'default': None,
+                'description': 'Set the parameter C of class i to class_weight[i]*C for SVC'},
+            'verbose': {
+                'type': 'boolean',
+                'default': False,
+                'description': 'Enable verbose output.'},
+            'max_iter': {
+                'type': 'integer',
+                'default': -1,
+                'description': 'Hard limit on iterations within solver, or -1 for no limit.'},
+            'decision_function_shape': {
+                'enum': ['ovo', 'ovr'],
+                'default': 'ovr',
+                'description': "Whether to return a one-vs-rest ('ovr') decision function of shape"},
+            'random_state': {
+                'anyOf': [{
+                    'type': 'integer'}, {
+                    'type': 'object'}, {
+                    'enum': [None]}],
+                'default': None,
+                'description': 'The seed of the pseudo random number generator used when shuffling'},
+        }},
+        {'description': 'gamma only used when kernel is ‘rbf’, ‘poly’ or ‘sigmoid’',
+         'anyOf': [{
+             'type': 'object',
+             'properties': {
+                 'kernel': {
+                     'enum': ['rbf', 'poly', 'sigmoid']},
+             }}, {
+             'type': 'object',
+             'properties': {
+                 'gamma': {
+                     'enum': ['auto_deprecated']}, #this needs to be the default value of gamma, changes with sklearn versions.
+             }}]},
+        {'description': 'coef0 only significant in kernel ‘poly’ and ‘sigmoid’.',
+         'anyOf': [{
+             'type': 'object',
+             'properties': {
+                 'kernel': {
+                     'enum': ['poly', 'sigmoid']},
+             }}, {
+             'type': 'object',
+             'properties': {
+                 'coef0': {
+                     'enum': [0.0]},
+             }}]}],
+}
+_input_fit_schema = {
+    '$schema': 'http://json-schema.org/draft-04/schema#',
+    'description': 'Fit the SVM model according to the given training data.',
+    'type': 'object',
+    'required': ['X', 'y'],
+    'properties': {
+        'X': {
+            'type': 'array',
+            'items': {'type': 'array', 'items': {'type': 'number'}},
+            'description': 'Training vectors, where n_samples is the number of samples and n_features is the number of features.'},
+        'y': {
+            'type': 'array',
+            'items': {'type': 'number'},
+            'description': 'Target values (class labels in classification, real numbers in regression)'},
+        'sample_weight': {
+            'anyOf': [{
+                    'type': 'array',
+                    'items': {'type': 'number'},
+                    'description': 'Per-sample weights. Rescale C per sample.'},
+                {'enum': [None]}],
+            'default': None,
+        },
+    },
+}
+_input_predict_schema = {
+    '$schema': 'http://json-schema.org/draft-04/schema#',
+    'description': 'Perform classification on samples in X.',
+    'type': 'object',
+    'required': ['X'],
+    'properties': {
+        'X': {
+            'type': 'array',
+            'items': {'type': 'array', 'items': {'type': 'number'}}
+        },
+    },
+}
+_output_predict_schema = {
+    '$schema': 'http://json-schema.org/draft-04/schema#',
+    'type': 'array',
+    'items':  {'type': 'number'},
+    'description': 'Class labels for samples in X.',
+}
+_input_predict_proba_schema = {
+    '$schema': 'http://json-schema.org/draft-04/schema#',
+    'type': 'object',
+    'required': ['X'],
+    'properties': {
+        'X': {
+            'type': 'array',
+            'items': {'type': 'array', 'items': {'type': 'number'}},
+        },
+    },
+}
+_output_predict_proba_schema = {
+    '$schema': 'http://json-schema.org/draft-04/schema#',
+    'type': 'array',
+    'items': {'type': 'array', 'items': {'type': 'number'}},
+    'description': 'Returns the probability of the sample for each class in the model.'
+
+}
+_combined_schemas = {
+    '$schema': 'http://json-schema.org/draft-04/schema#',
+    'description': 'Combined schema for expected data and hyperparameters.',
+    'type': 'object',
+    'tags': {
+        'pre': [],
+        'op': ['estimator'],
+        'post': []},
+    'properties': {
+        'hyperparams': _hyperparams_schema,
+        'input_fit': _input_fit_schema,
+        'input_predict': _input_predict_schema,
+        'output': _output_predict_schema,
+        'input_predict_proba': _input_predict_proba_schema,
+        'output_predict_proba': _output_predict_proba_schema},
+}
+if (__name__ == '__main__'):
+    lale.helpers.validate_is_schema(_combined_schemas)
+SVC = lale.operators.make_operator(SVCImpl, _combined_schemas)
---
+++ lale/search/SMAC.py
@@ -0,0 +1,177 @@
+# Copyright 2019 IBM Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import random
+import math
+import logging
+import numpy
+import warnings
+
+from typing import Any, Dict, List, Set, Iterable, Iterator, Optional, Tuple
+from hyperopt import hp
+from hyperopt.pyll import scope
+from lale.schema_simplifier import findRelevantFields, narrowToGivenRelevantFields, simplify, filterForOptimizer
+
+from lale.search.search_space_grid import get_search_space_grids, SearchSpaceGrid
+from lale.sklearn_compat import make_sklearn_compat
+
+from lale.schema_utils import Schema, getMinimum, getMaximum
+from lale.search.search_space import SearchSpace, SearchSpaceObject, SearchSpaceEnum, SearchSpaceNumber, SearchSpaceArray
+from lale.search.schema2search_space import schemaToSearchSpace
+from lale.search.PGO import PGO
+import numpy as np
+
+from smac.configspace import ConfigurationSpace
+from ConfigSpace.hyperparameters import CategoricalHyperparameter, \
+    UniformFloatHyperparameter, UniformIntegerHyperparameter, Hyperparameter
+from ConfigSpace.conditions import EqualsCondition
+
+logging.basicConfig(level=logging.WARNING)
+logger = logging.getLogger(__name__)
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    import lale.operators as Ops
+
+def lale_op_smac_tae(op:'Ops.PlannedOperator', f_min):
+    # TODO: we can probably do this in a different way, but get_smac_configuration_space
+    # we already have these sklearn compatibility wrappers it is easier for now to use them
+    op_compat = make_sklearn_compat(op)
+
+    def f(cfg):
+        from sklearn.base import clone
+        wrapped_op = clone(op_compat)
+        cfg2 = smac_fixup_params(cfg)
+#        print(cfg)
+        trainable = wrapped_op.set_params(**cfg2)
+
+        return f_min(trainable)
+        
+    return f
+
+def lale_trainable_op_from_config(op:'Ops.PlannedOperator', cfg)->'Ops.TrainableOperator':
+    from sklearn.base import clone
+
+    op_compat = make_sklearn_compat(op)
+
+    wrapped_op = clone(op_compat)
+    cfg2 = smac_fixup_params(cfg)
+    trainable = wrapped_op.set_params(**cfg2)
+    return trainable
+
+def get_smac_space(op:'Ops.PlannedOperator',
+                   lale_num_grids:Optional[float]=None,
+                   lale_pgo:Optional[PGO]=None)->ConfigurationSpace:
+    """ Top level function: given a lale operator, returns a ConfigurationSpace for use with SMAC
+    Parameters
+    ----------
+    op : The lale PlannedOperator
+    lale_num_grids: integer or float, optional
+        if set to an integer => 1, it will determine how many parameter grids will be returned (at most)
+        if set to an float between 0 and 1, it will determine what fraction should be returned
+        note that setting it to 1 is treated as in integer.  To return all results, use None
+    """
+    
+    hp_grids = get_search_space_grids(op, num_grids=lale_num_grids, pgo=lale_pgo)
+    grids = hp_grids_to_smac_cs(hp_grids)
+    return grids
+
+def smac_fixup_params(cfg):
+    def strip_key(k:str)->str:
+        return k.rsplit('_', 1)[0]
+    def transform_value(v):
+        if v == '_lale_none':
+            return None
+        else:
+            return v
+
+    ret = {strip_key(k):transform_value(v) for (k,v) in cfg.get_dictionary().items() if k != 'disjunct_discriminant'}
+    return ret
+
+# When sampling from distributions, this is the default number of samples to take.
+# Users can override this by passing in num_samples to the appropriate function
+SAMPLES_PER_DISTRIBUTION=2
+
+# We can first convert from our search space IR
+# to a more limited grid structure
+# This can than be converted to the format required for SMAC
+
+def SearchSpaceNumberToSMAC(key:str, hp:SearchSpaceNumber)->Hyperparameter:
+    """Returns either a list of values intended to be sampled uniformly or a frozen scipy.stats distribution
+    """
+    dist = "uniform"
+    if hp.distribution:
+        dist = hp.distribution    
+    if hp.maximum is None:
+        raise ValueError(f"maximum not specified for a number with distribution {dist} for {key}")
+    max = hp.getInclusiveMax()
+    if hp.minimum is None:
+        raise ValueError(f"minimum not specified for a number with distribution {dist} for {key}")
+    min = hp.getInclusiveMin()
+
+    log:bool
+    if dist == "uniform" or dist == "integer":
+        log = False
+    elif dist == "loguniform":
+        log = True
+    else:
+        raise ValueError(f"unknown/unsupported distribution {dist} for {key}")
+
+    if hp.discrete:
+        return UniformIntegerHyperparameter(key, min, max, log=log)
+    else:
+        return UniformFloatHyperparameter(key, min, max, log=log)
+
+class FakeNone(object):
+    pass
+
+MyFakeNone = FakeNone()
+
+def HPValuetoSMAC(key:str, hp:SearchSpace)->Hyperparameter:
+    def val_to_str(v):
+        if v is None:
+            return "_lale_none"
+        else:
+            return v
+
+    if isinstance(hp, SearchSpaceEnum):
+        return CategoricalHyperparameter(key, list(map(val_to_str, hp.vals)))
+    elif isinstance(hp, SearchSpaceNumber):
+        return SearchSpaceNumberToSMAC(key, hp)
+    elif isinstance(hp, SearchSpaceArray):
+        raise ValueError(f"Arrays are not yet supported by the SMAC backend (key: {key})")
+    else:
+        raise ValueError(f"Not yet supported hp description ({type(hp)}) (key: {key}) in the GridSearchCV backend")
+    
+def SearchSpaceGridtoSMAC(hp:SearchSpaceGrid, disc:int)->Iterable[Hyperparameter]:
+    return (HPValuetoSMAC(f"{k}_{disc}" , v) for k,v in hp.items())
+
+disc_str = "disjunct_discriminant"
+def addSearchSpaceGrid(hp:SearchSpaceGrid, disc:int, parent_disc:Hyperparameter, cs:ConfigurationSpace)->None:
+    smac = SearchSpaceGridtoSMAC(hp, disc)
+    for hyp in smac:
+        cs.add_hyperparameter(hyp)
+        cs.add_condition(EqualsCondition(child=hyp, parent=parent_disc, value=disc))
+
+def addSearchSpaceGrids(grids:List[SearchSpaceGrid], cs:ConfigurationSpace)->None:
+    parent_disc = CategoricalHyperparameter(disc_str, range(len(grids)))
+    cs.add_hyperparameter(parent_disc)
+    for (i,g) in enumerate(grids):
+        addSearchSpaceGrid(g, i, parent_disc, cs)
+
+def hp_grids_to_smac_cs(grids:List[SearchSpaceGrid])->ConfigurationSpace:
+    cs:ConfigurationSpace = ConfigurationSpace()
+    addSearchSpaceGrids(grids, cs)
+    return cs
