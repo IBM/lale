bf5cdb9456a358073ede8d4898a4732fc9b329f5
Martin Hirzel <hirzel@gmail.com>
2019-08-16 16:26:34 -0400

    Added missing documentation URLs to scikit-learn operators. Tested `PCA & NoOp`, and since it worked, used it for a more realistic example in demo_dynamic_schemas.ipynb. Tried BERT for the text+structured demo, and generalized it for single-column tables. (#15)

--- lale/lib/sklearn/quadratic_discriminant_analysis.py
+++ lale/lib/sklearn/quadratic_discriminant_analysis.py
@@ -143,6 +143,7 @@ _output_predict_proba_schema = {
 _combined_schemas = {
     '$schema': 'http://json-schema.org/draft-04/schema#',
     'description': 'Combined schema for expected data and hyperparameters.',
+    'documentation_url': 'https://scikit-learn.org/stable/modules/generated/sklearn.discriminant_analysis.QuadraticDiscriminantAnalysis.html',
     'type': 'object',
     'tags': {
         'pre': [],
--- lale/lib/sklearn/svc.py
+++ lale/lib/sklearn/svc.py
@@ -225,6 +225,7 @@ _output_predict_proba_schema = {
 _combined_schemas = {
     '$schema': 'http://json-schema.org/draft-04/schema#',
     'description': 'Combined schema for expected data and hyperparameters.',
+    'documentation_url': 'https://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html',
     'type': 'object',
     'tags': {
         'pre': [],
