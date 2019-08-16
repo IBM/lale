89254d2f0ec83a099b8d4d4eb2c2aeba3b0712c9
Martin Hirzel <hirzel@gmail.com>
2019-08-16 16:26:34 -0400

    Added missing documentation URLs to scikit-learn operators. Tested `PCA & NoOp`, and since it worked, used it for a more realistic example in demo_dynamic_schemas.ipynb. Tried BERT for the text+structured demo, and generalized it for single-column tables. (#15)

--- lale/lib/sklearn/passive_aggressive_classifier.py
+++ lale/lib/sklearn/passive_aggressive_classifier.py
@@ -217,6 +217,7 @@ _output_schema = {
 _combined_schemas = {
     '$schema': 'http://json-schema.org/draft-04/schema#',
     'description': 'Combined schema for expected data and hyperparameters.',
+    'documentation_url': 'https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.PassiveAggressiveClassifier.html',
     'type': 'object',
     'tags': {
         'pre': [],
