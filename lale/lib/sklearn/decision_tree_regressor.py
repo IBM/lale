4df01a4572b50be73ed6b63c372249cbc074b7d2
Martin Hirzel <hirzel@gmail.com>
2019-12-02 17:12:31 -0500

    Heavily revised scikit-learn guide. (#91)

--- lale/lib/sklearn/decision_tree_regressor.py
+++ lale/lib/sklearn/decision_tree_regressor.py
@@ -43,101 +43,102 @@ class DecisionTreeRegressorImpl():
 
     def predict(self, X):
         return self._sklearn_model.predict(X)
+
 _hyperparams_schema = {
     '$schema': 'http://json-schema.org/draft-04/schema#',
     'description': 'A decision tree regressor.',
-    'allOf': [{
-        'type': 'object',
+    'allOf': [
+    {   'type': 'object',
         'required': ['criterion', 'splitter', 'max_depth', 'min_samples_split', 'min_samples_leaf', 'max_features'],
         'relevantToOptimizer': ['criterion', 'splitter', 'max_depth', 'min_samples_split', 'min_samples_leaf', 'max_features'],
         'additionalProperties': False,
         'properties': {
             'criterion': {
+                'description': 'Function to measure the quality of a split.',
                 'enum': ['mse', 'friedman_mse', 'mae'],
-                'default': 'mse',
-                'description': 'The function to measure the quality of a split. Supported criteria'},
+                'default': 'mse'},
             'splitter': {
                 'enum': ['best', 'random'],
                 'default': 'best',
-                'description': 'The strategy used to choose the split at each node. Supported'},
+                'description': 'Strategy to choose the split at each node.'},
             'max_depth': {
-                'description': 'The maximum depth of the tree.',
+                'description': 'Maximum depth of the tree.',
+                'default': None,
                 'anyOf': [
                 {   'type': 'integer',
+                    'minimum': 1,
                     'minimumForOptimizer': 3,
                     'maximumForOptimizer': 5},
-                {   'description': 'Nodes are expanded until all leaves are pure or until all leaves contain less than min_samples_split samples.',
-                    'enum': [None]}],
-                'default': None},
+                {   'enum': [None],
+                    'description': 'If None, then nodes are expanded until all leaves are pure, or until all leaves contain less than min_samples_split samples.'}]},
             'min_samples_split': {
-                'anyOf': [{
-                    'type': 'integer',
+                'description': 'Minimum number of samples required to split an internal node.',
+                'anyOf': [
+                {   'type': 'integer',
                     'minimumForOptimizer': 2,
                     'maximumForOptimizer': 20,
-                    'distribution': 'uniform'}, {
-                    'type': 'number',
+                    'distribution': 'uniform'},
+                {   'type': 'number',
                     'minimumForOptimizer': 0.01,
                     'maximumForOptimizer': 0.5}],
-                'default': 2,
-                'description': 'The minimum number of samples required to split an internal node:'},
+                'default': 2},
             'min_samples_leaf': {
-                'anyOf': [{
-                    'type': 'integer',
+                'description': 'Minimum number of samples required to be at a leaf node.',
+                'anyOf': [
+                {   'type': 'integer',
                     'minimumForOptimizer': 1,
                     'maximumForOptimizer': 20,
-                    'distribution': 'uniform'}, {
-                    'type': 'number',
+                    'distribution': 'uniform'},
+                {   'type': 'number',
                     'minimumForOptimizer': 0.01,
                     'maximumForOptimizer': 0.5}],
-                'default': 1,
-                'description': 'The minimum number of samples required to be at a leaf node.'},
+                'default': 1},
             'min_weight_fraction_leaf': {
                 'type': 'number',
                 'default': 0.0,
-                'description': 'The minimum weighted fraction of the sum total of weights (of all'},
+                'description': 'Minimum weighted fraction of the sum total of weights (of all the input samples) required to be at a leaf node.'},
             'max_features': {
-                'anyOf': [{
-                    'type': 'integer',
-                    'forOptimizer': False}, {
-                    'type': 'number',
+                'description': 'The number of features to consider when looking for the best split.',
+                'anyOf': [
+                {   'type': 'integer',
+                    'forOptimizer': False},
+                {   'type': 'number',
                     'minimum': 0.0,
                     'exclusiveMinimum': True,
                     'minimumForOptimizer': 0.0,
                     'maximumForOptimizer': 1.0,
-                    'distribution': 'uniform'}, {
-                    'enum': ['auto', 'sqrt', 'log2', None]}],
-                'default': None,
-                'description': 'The number of features to consider when looking for the best split:'},
+                    'distribution': 'uniform'},
+                {   'enum': ['auto', 'sqrt', 'log2', None]}],
+                'default': None},
             'random_state': {
-                'anyOf': [{
-                    'type': 'integer'}, {
-                    'type': 'object'}, {
-                    'enum': [None]}],
-                'default': None,
-                'description': 'If int, random_state is the seed used by the random number generator;'},
+                'anyOf': [
+                {   'type': 'integer'},
+                {   'type': 'object'},
+                {   'enum': [None]}],
+                'default': None},
             'max_leaf_nodes': {
-                'anyOf': [{
-                    'type': 'integer'}, {
-                    'enum': [None]}],
+                'anyOf': [
+                {   'type': 'integer'},
+                {   'enum': [None]}],
                 'default': None,
-                'description': 'Grow a tree with ``max_leaf_nodes`` in best-first fashion.'},
+                'description': 'Grow a tree with `max_leaf_nodes` in best-first fashion.'},
             'min_impurity_decrease': {
                 'type': 'number',
                 'default': 0.0,
-                'description': 'A node will be split if this split induces a decrease of the impurity'},
+                'description': 'A node will be split if this split induces a decrease of the impurity greater than or equal to this value.'},
             'min_impurity_split': {
                 'anyOf':[
                 {'type': 'number'},{
                     'enum': [None]
                 }],
                 'default': None,
-                'description': 'Threshold for early stopping in tree growth. A node will split'},
+                'description': 'Threshold for early stopping in tree growth.'},
             'presort': {
                 'type': 'boolean',
                 'default': False,
-                'description': 'Whether to presort the data to speed up the finding of best splits in'},
-        }}]
-}
+                'description': 'Whether to presort the data to speed up the finding of best splits in fitting.'},
+        }}]}
+
 _input_fit_schema = {
     '$schema': 'http://json-schema.org/draft-04/schema#',
     'description': 'Build a decision tree regressor from the training set (X, y).',
