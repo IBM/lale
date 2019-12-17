3e462578c327dc25df90f8c71b2de2df5024414c
Avi Shinnar <shinnar@us.ibm.com>
2019-12-17 15:28:50 -0500

    add forOptimizer helper that creates a schema as the optimizer would see it (with forOptimizer stuff 'promoted')

--- lale/schema_utils.py
+++ lale/schema_utils.py
@@ -21,6 +21,9 @@ SchemaEnum = Set[Any]
 STrue:Schema = {}
 SFalse:Schema = {"not":STrue}
 
+forOptimizerConstant:str = 'forOptimizer'
+forOptimizerConstantSuffix:str = 'ForOptimizer'
+
 def is_true_schema(s:Schema)->bool:
     return s is True or s == STrue
 
@@ -28,7 +31,7 @@ def is_false_schema(s:Schema)->bool:
     return s is False or s == SFalse
 
 def getForOptimizer(obj, prop:str):
-    return obj.get(prop + 'ForOptimizer', None)
+    return obj.get(prop + forOptimizerConstantSuffix, None)
 
 def getMinimum(obj):
     prop = 'minimum'
@@ -70,8 +73,97 @@ def getExclusiveMaximum(obj):
     else:
         return mfo
 
-def forOptimizer(s:Schema)->bool:
+
+def isForOptimizer(s:Schema)->bool:
     if isinstance(s, dict):
-        return s.get('forOptimizer', True)
+        return s.get(forOptimizerConstant, True)
     else:
         return True
+
+def makeSingleton_(k:str, schemas:List[Schema])->Schema:
+    if len(schemas) == 0:
+        return {}
+    if len(schemas) == 1:
+        return schemas[0]
+    else:
+        return {k:schemas}
+
+def makeAllOf(schemas:List[Schema])->Schema:
+    return makeSingleton_('allOf', schemas)
+def makeAnyOf(schemas:List[Schema])->Schema:
+    return makeSingleton_('anyOf', schemas)
+def makeOneOf(schemas:List[Schema])->Schema:
+    return makeSingleton_('oneOf', schemas)
+
+def forOptimizer(schema:Schema)->Schema:
+    if schema is None or schema is True or schema is False:
+        return schema
+    if not isForOptimizer(schema):
+        return None
+    if 'anyOf' in schema:
+        subs = schema['anyOf']
+        sch = [forOptimizer(s) for s in subs]
+        sch_nnil = [s for s in sch if s is not None]
+        if sch_nnil:
+            return makeAnyOf(sch_nnil)
+        else:
+            return None
+    if 'allOf' in schema:
+        subs = schema['allOf']
+        sch = [forOptimizer(s) for s in subs]
+        sch_nnil = [s for s in sch if s is not None]
+        filtered_sch = sch_nnil
+        if len(sch_nnil) != len(sch):
+            # Questionable semantics here (aka HACK!!!!)
+            # Since we removed something from the schema
+            # we will also remove negated schemas
+            filtered_sch = [s for s in sch_nnil if not isinstance(s, dict) or 'not' not in s]
+
+        if filtered_sch:
+            return makeAllOf(filtered_sch)
+        else:
+            return None
+    if 'oneOf' in schema:
+        subs = schema['oneOf']
+        sch = [forOptimizer(s) for s in subs]
+        sch_nnil = [s for s in sch if s is not None]
+        if sch_nnil:
+            return makeOneOf(sch_nnil)
+        else:
+            return None
+
+    if 'not' in schema:
+        s = forOptimizer(schema['not'])
+        if s is None:
+            return None
+        else:
+            return {'not':s}
+
+    transformedSchema:Schema = {}
+    for k,v in schema.items():
+        if k.endswith(forOptimizerConstantSuffix):
+            base:str = k[:-len(forOptimizerConstantSuffix)]
+            transformedSchema[base] = v
+        elif k not in transformedSchema:
+            transformedSchema[k] = v
+
+    schema = transformedSchema
+    if 'type' in schema and schema['type'] == 'object' and 'properties' in schema:
+        required = schema.get('required', None)
+
+        props = {}
+        for k,v in schema['properties'].items():
+            s = forOptimizer(v)
+            if s is None:
+#                if required and k in required:
+                    # if this field is required (and has now been filtered)
+                    # filter the whole object schema
+                    return None
+            else:
+                props[k] = s
+
+        ret = schema.copy()
+        ret['properties'] = props
+        return ret
+
+    return schema
