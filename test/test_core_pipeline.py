7294148eb4156fb447651514d35eb36ac97bf478
kakate@us.ibm.com <kakate@us.ibm.com>
2020-02-21 16:01:18 -0500

    Moving batching test cases to test with lale[full].

--- test/test_core_pipeline.py
+++ test/test_core_pipeline.py
@@ -22,7 +22,6 @@ import pickle
 
 from lale.lib.lale import ConcatFeatures
 from lale.lib.lale import NoOp
-from lale.lib.lale import Batching
 from lale.lib.sklearn import KNeighborsClassifier
 from lale.lib.sklearn import LinearSVC
 from lale.lib.sklearn import LogisticRegression
