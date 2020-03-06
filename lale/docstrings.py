d79925b4f858fec85ac350705a75e8e5d0bcd028
Martin Hirzel <hirzel@gmail.com>
2020-03-06 18:51:37 -0500

    Trying to fix missing line-breaks in readthedocs by using nested lists for subschema components. (#214)

--- lale/docstrings.py
+++ lale/docstrings.py
@@ -1,11 +1,87 @@
 import lale.helpers
 import lale.pretty_print
+import pprint
 
-def add_indent(prefix, string):
+def _indent(prefix, string, first_prefix=None):
     lines = string.splitlines()
-    indented = [prefix + line for line in lines]
-    result = '\n'.join(indented)
+    if first_prefix is None:
+        first_prefix = prefix
+    first_indented = (first_prefix + lines[0]).rstrip()
+    rest_indented = [(prefix + line).rstrip() for line in lines[1:]]
+    result = first_indented + '\n' + '\n'.join(rest_indented)
     return result
+
+def _value_docstring(value):
+    return pprint.pformat(value, width=10000, compact=True)
+
+def _kind_tag(schema):
+    if 'type' in schema:
+        if 'typeForOptimizer' in schema:
+            return schema['typeForOptimizer']
+        elif schema['type'] == 'object':
+            return 'dict'
+        else:
+            return schema['type']
+    elif 'enum' in schema:
+        values = schema['enum']
+        assert len(values) >= 1
+        if len(values) == 1:
+            return _value_docstring(values[0])
+        elif len(values) == 2:
+            return ' or '.join([_value_docstring[v] for v in values])
+        else:
+            prefix = ', '.join([_value_docstring[v] for v in values[:-1]])
+            suffix = ' or ' + _value_docstring[values[-1]]
+            return prefix + suffix
+    elif 'anyOf' in schema:
+        return 'union type'
+    elif 'allOf' in schema:
+        return 'intersection type'
+    elif 'not' in schema:
+        return 'negated type'
+    else:
+        return 'any type'
+
+def _schema_docstring(name, schema, required=True, relevant=True):
+    tags = [_kind_tag(schema)]
+    #TODO: for numbers, keywords minimum, maximum, exclusive{Minimum,Maximum}
+    #TODO: for numbers, distribution
+    #TODO: for arrays, {min,max}Items, {min,max}ItemsForOptimizer
+    if not required:
+        tags.append('optional')
+    if not relevant or schema.get('forOptimizer', False):
+        tags.append('not for optimizer')
+    if 'default' in schema:
+        tags.append('default ' + _value_docstring(schema['default']))
+    result = name + ' : ' if name else ''
+    result += ', '.join(tags)
+    assert len(result) > 0 and result.rstrip() == result
+    if 'description' in schema:
+        result += '\n\n' + _indent('  ', schema['description']).rstrip()
+    def item_docstring(name, item_schema):
+        sd = _schema_docstring(name, item_schema)
+        return _indent('    ', sd, '  - ').rstrip()
+    body = None
+    if 'anyOf' in schema:
+        item_docstrings = [item_docstring(None, s) for s in schema['anyOf']]
+        body = '\n\n'.join(item_docstrings)
+    elif 'allOf' in schema:
+        item_docstrings = [item_docstring(None, s) for s in schema['allOf']]
+        body = '\n\n'.join(item_docstrings)
+    elif 'not' in schema:
+        body = item_docstring(None, schema['not'])
+    elif schema.get('type', '') == 'array':
+        items_schemas = schema['items']
+        assert isinstance(items_schemas, dict), 'TODO: array items as list'
+        body = item_docstring('items', items_schemas)
+    elif schema.get('type', '') == 'object' and 'properties' in schema:
+        #TODO: pass down info on which properties are required
+        item_docstrings = [item_docstring(k, s)
+                           for k, s in schema['properties'].items()]
+        body = '\n\n'.join(item_docstrings)
+    if body is not None:
+        result += '\n\n' + body
+    return result.rstrip()
 
 def _params_docstring(params_schema):
     if len(params_schema['properties']) == 0:
@@ -13,23 +89,12 @@ def _params_docstring(params_schema):
     else:
         result = 'Parameters\n----------\n'
     for param_name, param_schema in params_schema['properties'].items():
-        result += param_name + ': '
-        tags = []
-        if 'relevantToOptimizer' in params_schema:
-            if param_name in params_schema['relevantToOptimizer']:
-                tags.append('relevant to optimizer')
-            else:
-                tags.append('not tuned by optimizer')
-        if 'required' in params_schema:
-            if param_name not in params_schema['required']:
-                tags.append('optional')
-        result += ', '.join(tags)
-        result += '\n'
-        result += add_indent('  ', param_schema['description'])
-        result += '\n\n  ::\n\n'
-        schema = lale.helpers.dict_without(param_schema, 'description')
-        schema_string = lale.pretty_print.schema_to_string(schema)
-        result += add_indent('    ', 'schema = ' + schema_string)
+        required = param_name in params_schema.get('required', {})
+        relevant = ('relevantToOptimizer' not in params_schema
+                    or param_name in params_schema['relevantToOptimizer'])
+        item_docstring = _schema_docstring(
+            param_name, param_schema, required, relevant)
+        result += _indent('  ', item_docstring, '')
         result += '\n\n'
     return result
 
@@ -37,12 +102,10 @@ def _method_docstring(description, params_schema, result_schema=None):
     result = description + '\n\n'
     result += _params_docstring(params_schema)
     if result_schema is not None:
-        result += 'Returns\n-------\nSee schema.\n'
-        result += add_indent('  ', result_schema['description'])
-        result += '\n\n  ::\n\n'
-        schema = lale.helpers.dict_without(result_schema, 'description')
-        schema_string = lale.pretty_print.schema_to_string(schema)
-        result += add_indent('    ', 'schema = ' + schema_string)
+        result += 'Returns\n-------\n'
+        item_docstring = _schema_docstring('result', result_schema)
+        result += _indent('  ', item_docstring, '')
+        result += '\n\n'
     return result
 
 def _hyperparams_docstring(hyperparams_schema):
