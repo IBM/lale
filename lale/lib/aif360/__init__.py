a7d9fcbfb11be6f2b6f1e0c1387fde67b379edde
Martin Hirzel <hirzel@gmail.com>
2020-04-13 16:17:48 -0400

    Do not raise an assertion when docstrings are set multiple times for method inherited from common superclass. (#288)

--- lale/lib/aif360/__init__.py
+++ lale/lib/aif360/__init__.py
@@ -12,6 +12,36 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""
+Scikit-learn compatible wrappers for a subset of the operators from AIF360_ along with schemas to enable hyperparameter tuning.
+
+.. _AIF360: https://github.com/IBM/AIF360
+
+Operators:
+==========
+* `CalibratedEqOddsPostprocessing`_
+* `DisparateImpactRemover`_
+* `EqOddsPostprocessing`_
+* `RejectOptionClassification`_
+
+.. _`CalibratedEqOddsPostprocessing`: lale.lib.aif360.calibrated_eq_odds_postprocessing.html
+.. _`DisparateImpactRemover`: lale.lib.aif360.disparate_impact_remover.html
+.. _`EqOddsPostprocessing`: lale.lib.aif360.eq_odds_postprocessing.html
+.. _`RejectOptionClassification`: lale.lib.aif360.reject_option_classification.html
+
+Functions:
+==========
+* `dataset_fairness_info`_
+* `dataset_to_pandas`_
+* `disparate_impact`_
+* `statistical_parity_difference`_
+
+.. _`dataset_fairness_info`: lale.lib.aif360.util.html#lale.lib.aif360.util.dataset_fairness_info
+.. _`dataset_to_pandas`: lale.lib.aif360.util.html#lale.lib.aif360.util.dataset_to_pandas
+.. _`disparate_impact`: lale.lib.aif360.util.html#lale.lib.aif360.util.disparate_impact
+.. _`statistical_parity_difference`: lale.lib.aif360.util.html#lale.lib.aif360.util.statistical_parity_difference
+"""
+
 from .calibrated_eq_odds_postprocessing import CalibratedEqOddsPostprocessing
 from .disparate_impact_remover import DisparateImpactRemover
 from .eq_odds_postprocessing import EqOddsPostprocessing
