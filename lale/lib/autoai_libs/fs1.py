feca9fc403ee6d9b30cd95389d0937807ef08f0d
kakate@us.ibm.com <kiran.kate@gmail.com>
2020-04-21 18:33:51 -0400

    Corrected data type for cols_ids_must_keep in FS1.

--- lale/lib/autoai_libs/fs1.py
+++ lale/lib/autoai_libs/fs1.py
@@ -42,8 +42,7 @@ _hyperparams_schema = {
         'properties': {
             'cols_ids_must_keep': {
                 'description': 'Serial numbers of the columns that must be kept irrespective of their feature importance.',
-                'type': 'array',
-                'items': {'type': 'integer', 'minimum': 0},
+                'laleType':'Any',#Found a value `range(0, 20)` 
                 'default': []},
             'additional_col_count_to_keep': {
                 'description': 'How many columns need to be retained.',
