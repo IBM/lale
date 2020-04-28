ac2cbf95d95a7c83d7326fa6ac11b4678c349ff7
kakate@us.ibm.com <kakate@us.ibm.com>
2020-04-28 05:16:58 -0400

    Released version 0.3.8 on PyPI.

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ else:
 
 setup(
     name='lale',
-    version='0.3.7',
+    version='0.3.8',
     author="Guillaume Baudart, Martin Hirzel, Kiran Kate, Parikshit Ram, Avraham Shinnar",
     description="Library for Semi-Automated Data Science",
     long_description=long_description,
