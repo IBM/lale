02a12ea2289d1dafb5e50d209fbf46e969c40ddc
kakate@us.ibm.com <kakate@us.ibm.com>
2020-05-13 16:06:17 -0400

    Handle the case of IsolationForestAnomaly which return None from fit.

--- lale/lib/autoai_libs/tam.py
+++ lale/lib/autoai_libs/tam.py
@@ -34,7 +34,9 @@ class TAMImpl():
         else:
             self._wrapped_model.fit(X, y, **fit_params)
         cands = self._wrapped_model.get_candidates(X, self._wrapped_model.col_names, self._wrapped_model.col_dtypes)
-        self._wrapped_model.trans_class_obj_ = self._wrapped_model.trans_class_obj_.fit(X[:, list(cands[0])])
+        trained_obj = self._wrapped_model.trans_class_obj_.fit(X[:, list(cands[0])])
+        if trained_obj is not None:
+            self._wrapped_model.trans_class_obj_ = trained_obj
         return self
 
     def transform(self, X):
