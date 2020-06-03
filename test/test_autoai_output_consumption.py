063a0382c89edf2146b80097d7b5ead1d7a1c6b4
kakate@us.ibm.com <kiran.kate@gmail.com>
2020-06-03 16:21:51 -0400

    Corrected the renaming.

--- test/test_autoai_output_consumption.py
+++ test/test_autoai_output_consumption.py
@@ -26,36 +26,36 @@ class TestAutoAIOutputConsumption(unittest.TestCase):
     def setUp(cls) -> None:
         urllib.request.urlretrieve(cls.pickled_model_url, cls.pickled_model_path)
         urllib.request.urlretrieve(cls.train_csv_url, cls.train_csv_path)
-        TestLaleContract.training_df = pd.read_csv(TestLaleContract.train_csv_path)
+        TestAutoAIOutputConsumption.training_df = pd.read_csv(TestAutoAIOutputConsumption.train_csv_path)
 
     def test_01_load_pickled_model(self):
         try:
-            TestLaleContract.model = joblib.load(TestLaleContract.pickled_model_path)
-            print(type(TestLaleContract.model))
-            print(TestLaleContract.model)
+            TestAutoAIOutputConsumption.model = joblib.load(TestAutoAIOutputConsumption.pickled_model_path)
+            print(type(TestAutoAIOutputConsumption.model))
+            print(TestAutoAIOutputConsumption.model)
         except Exception as e:
             assert False, f"Exception was thrown during model pickle: {e}"
 
     def test_02_predict_on_model(self):
-        check_X = TestLaleContract.training_df.drop(['Risk'], axis=1).values
+        check_X = TestAutoAIOutputConsumption.training_df.drop(['Risk'], axis=1).values
         x = check_X
         try:
-            pred = TestLaleContract.model.predict(x)
+            pred = TestAutoAIOutputConsumption.model.predict(x)
             assert len(pred) == len(x), f"Prediction has returned unexpected number of rows {len(pred)} - expected {len(x)}"
         except Exception as e:
             assert False, f"Exception was thrown during model prediction: {e}"
 
     def test_03_print_pipeline(self):
-        lale_pipeline = TestLaleContract.model
-        TestLaleContract.pipeline_content = lale_pipeline.pretty_print()
-        assert type(TestLaleContract.pipeline_content) == str
-        assert len(TestLaleContract.pipeline_content) > 0
-        assert "lale.wrap_imported_operators()" in TestLaleContract.pipeline_content
+        lale_pipeline = TestAutoAIOutputConsumption.model
+        TestAutoAIOutputConsumption.pipeline_content = lale_pipeline.pretty_print()
+        assert type(TestAutoAIOutputConsumption.pipeline_content) == str
+        assert len(TestAutoAIOutputConsumption.pipeline_content) > 0
+        assert "lale.wrap_imported_operators()" in TestAutoAIOutputConsumption.pipeline_content
 
     def test_04_execute_pipeline(self):
         try:
             with open("pp_pipeline.py", "w") as pipeline_f:
-                pipeline_f.write(TestLaleContract.pipeline_content)
+                pipeline_f.write(TestAutoAIOutputConsumption.pipeline_content)
 
             import importlib.util
             import lale.operators
@@ -63,8 +63,8 @@ class TestAutoAIOutputConsumption(unittest.TestCase):
             spec = importlib.util.spec_from_file_location("pp_pipeline", "pp_pipeline.py")
             pipeline_module = importlib.util.module_from_spec(spec)
             spec.loader.exec_module(pipeline_module)
-            TestLaleContract.pp_pipeline = pipeline_module.pipeline
-            assert(isinstance(TestLaleContract.pp_pipeline, lale.operators.TrainablePipeline))
+            TestAutoAIOutputConsumption.pp_pipeline = pipeline_module.pipeline
+            assert(isinstance(TestAutoAIOutputConsumption.pp_pipeline, lale.operators.TrainablePipeline))
         except Exception as e:
             assert False, f"{e}"
         finally:
@@ -74,16 +74,16 @@ class TestAutoAIOutputConsumption(unittest.TestCase):
                 print("Couldn't remove pp_pipeline.py file")
 
     def test_05_train_pretty_print_pipeline(self):
-        train_X = TestLaleContract.training_df.drop(['Risk'], axis=1).values
-        train_y = TestLaleContract.training_df.Risk.values
+        train_X = TestAutoAIOutputConsumption.training_df.drop(['Risk'], axis=1).values
+        train_y = TestAutoAIOutputConsumption.training_df.Risk.values
 
-        TestLaleContract.pp_pipeline.fit(train_X, train_y)
+        TestAutoAIOutputConsumption.pp_pipeline.fit(train_X, train_y)
 
     def test_06_predict_on_pretty_print_pipeline(self):
-        check_X = TestLaleContract.training_df.drop(['Risk'], axis=1).values
+        check_X = TestAutoAIOutputConsumption.training_df.drop(['Risk'], axis=1).values
         x = check_X
         try:
-            pred = TestLaleContract.pp_pipeline.predict(x)
+            pred = TestAutoAIOutputConsumption.pp_pipeline.predict(x)
             assert len(pred) == len(x), f"Prediction on pretty print model has returned unexpected number of rows {len(pred)} - expected {len(x)}"
         except Exception as e:
             assert False, f"Exception was thrown during pretty print model prediction: {e}"
@@ -91,9 +91,9 @@ class TestAutoAIOutputConsumption(unittest.TestCase):
     def test_07_convert_model_to_lale(self):
         import lale.operators
         try:
-            lale_pipeline = TestLaleContract.model
-            TestLaleContract.prefix_model = lale_pipeline.remove_last().freeze_trainable()
-            assert(isinstance(TestLaleContract.prefix_model, lale.operators.TrainablePipeline))
+            lale_pipeline = TestAutoAIOutputConsumption.model
+            TestAutoAIOutputConsumption.prefix_model = lale_pipeline.remove_last().freeze_trainable()
+            assert(isinstance(TestAutoAIOutputConsumption.prefix_model, lale.operators.TrainablePipeline))
         except Exception as e:
             assert False, f"Exception was thrown during model prediction: {e}"
 
@@ -103,23 +103,23 @@ class TestAutoAIOutputConsumption(unittest.TestCase):
 
         wrap_imported_operators()
         try:
-            print(f"prefix_model is of type: {type(TestLaleContract.prefix_model)}")
+            print(f"prefix_model is of type: {type(TestAutoAIOutputConsumption.prefix_model)}")
             print(f"LR is of type {type(LR)}")
             # This is for classifiers, regressors needs to have different operators & different scoring metrics (e.g 'r2')
-            new_model = TestLaleContract.prefix_model >> (LR | Tree | KNN)
-            train_X = TestLaleContract.training_df.drop(['Risk'], axis=1).values
-            train_y = TestLaleContract.training_df['Risk'].values
+            new_model = TestAutoAIOutputConsumption.prefix_model >> (LR | Tree | KNN)
+            train_X = TestAutoAIOutputConsumption.training_df.drop(['Risk'], axis=1).values
+            train_y = TestAutoAIOutputConsumption.training_df['Risk'].values
             hyperopt = Hyperopt(estimator=new_model, cv=2, max_evals=3, scoring='roc_auc')
             hyperopt_pipelines = hyperopt.fit(train_X, train_y)
-            TestLaleContract.refined_model = hyperopt_pipelines.get_pipeline()
+            TestAutoAIOutputConsumption.refined_model = hyperopt_pipelines.get_pipeline()
         except Exception as e:
             assert False, f"Exception was thrown during model refinery: {e}"
 
     def test_09_predict_refined_model(self):
-        check_X = TestLaleContract.training_df.drop(['Risk'], axis=1).values
+        check_X = TestAutoAIOutputConsumption.training_df.drop(['Risk'], axis=1).values
         x = check_X
         try:
-            pred = TestLaleContract.refined_model.predict(x)
+            pred = TestAutoAIOutputConsumption.refined_model.predict(x)
             assert len(pred) == len(
                 x), f"Prediction on refined model has returned unexpected number of rows {len(pred)} - expected {len(x)}"
         except Exception as e:
