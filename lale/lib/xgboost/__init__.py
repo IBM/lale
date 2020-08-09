ee04a5db4e53da762ffedd13a83acc31ed5ed37b
Martin Hirzel <hirzel@gmail.com>
2020-08-09 08:46:18 -0400

    Moved XGBoost from required dependencies to be only required for "ful… (#410)
    * Moved XGBoost from required dependencies to be only required for "full" install. Also, changed exception handling for importing a few things in an effort to improve docstring generation.
    
    * Same for LightGBM.

--- lale/lib/xgboost/__init__.py
+++ lale/lib/xgboost/__init__.py
@@ -12,5 +12,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""
+Scikit-learn compatible wrappers for XGBoost_ along with schemas to enable hyperparameter tuning.
+
+.. _XGBoost: https://xgboost.readthedocs.io/en/latest/
+
+Operators:
+==========
+* `XGBClassifier`_
+* `XGBRegressor`_
+
+.. _`XGBClassifier`: lale.lib.aif360.xgb_classifier.html
+.. _`XGBRegressor`: lale.lib.aif360.xgb_regressor.html
+"""
+
 from .xgb_classifier import XGBClassifier
 from .xgb_regressor import XGBRegressor
