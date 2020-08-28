2bbdfdb6692832b32a085d0db24e963e9b18f10f
Martin Hirzel <hirzel@us.ibm.com>
2020-08-27 20:01:18 -0400

    Help hyperopt search space generator recognize that the callable is disjoint.

--- lale/lib/sklearn/column_transformer.py
+++ lale/lib/sklearn/column_transformer.py
@@ -68,6 +68,7 @@ _hyperparams_schema = {
              { 'type': 'array', 'items': {'type': 'boolean'},
                'description': 'Boolean mask.'},
              { 'laleType': 'callable',
+               'not': {'type': ['integer', 'array', 'string']},
                'description': 'Callable that is passed the input data X and can return any of the above.'}]}]}},
       'remainder': {
         'description':
