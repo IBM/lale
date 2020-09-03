f6dea2b74b998964d4d04da7e1dac34990feb077
Martin Hirzel <hirzel@gmail.com>
2020-09-03 08:20:21 -0400

    Support sklearn 0.23 in RandomForestClassifier, ExtraTreesClassifier, and GradientBoostingClassifier. (#437)

--- lale/lib/sklearn/gradient_boosting_classifier.py
+++ lale/lib/sklearn/gradient_boosting_classifier.py
@@ -17,30 +17,8 @@ import lale.docstrings
 import lale.operators
 
 class GradientBoostingClassifierImpl():
-
-    def __init__(self, loss='deviance', learning_rate=0.1, n_estimators=100, subsample=1.0, criterion='friedman_mse', min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_depth=3, min_impurity_decrease=0.0, min_impurity_split=None, init=None, random_state=None, max_features=None, verbose=0, max_leaf_nodes=None, warm_start=False, presort=None, validation_fraction=0.1, n_iter_no_change=None, tol=0.0001):
-        self._hyperparams = {
-            'loss': loss,
-            'learning_rate': learning_rate,
-            'n_estimators': n_estimators,
-            'subsample': subsample,
-            'criterion': criterion,
-            'min_samples_split': min_samples_split,
-            'min_samples_leaf': min_samples_leaf,
-            'min_weight_fraction_leaf': min_weight_fraction_leaf,
-            'max_depth': max_depth,
-            'min_impurity_decrease': min_impurity_decrease,
-            'min_impurity_split': min_impurity_split,
-            'init': init,
-            'random_state': random_state,
-            'max_features': max_features,
-            'verbose': verbose,
-            'max_leaf_nodes': max_leaf_nodes,
-            'warm_start': warm_start,
-            'presort': presort,
-            'validation_fraction': validation_fraction,
-            'n_iter_no_change': n_iter_no_change,
-            'tol': tol}
+    def __init__(self, **hyperparams):
+        self._hyperparams = hyperparams
         self._wrapped_model = sklearn.ensemble.GradientBoostingClassifier(**self._hyperparams)
 
     def fit(self, X, y, **fit_params):
@@ -333,6 +311,23 @@ _combined_schemas = {
         'output_decision_function': _output_decision_function_schema,
 }}
 
-lale.docstrings.set_docstrings(GradientBoostingClassifierImpl, _combined_schemas)
-
+GradientBoostingClassifier : lale.operators.IndividualOp
 GradientBoostingClassifier = lale.operators.make_operator(GradientBoostingClassifierImpl, _combined_schemas)
+
+if sklearn.__version__ >= '0.22':
+    # old: https://scikit-learn.org/0.20/modules/generated/sklearn.ensemble.GradientBoostingClassifier.html
+    # new: https://scikit-learn.org/0.23/modules/generated/sklearn.ensemble.GradientBoostingClassifier.html
+    from lale.schemas import AnyOf, Bool, Enum, Float
+    GradientBoostingClassifier = GradientBoostingClassifier.customize_schema(
+        presort=AnyOf(
+            types=[Bool(), Enum(['deprecated'])],
+            desc='This parameter is deprecated and will be removed in v0.24.',
+            default='deprecated'),
+        ccp_alpha=Float(
+            desc='Complexity parameter used for Minimal Cost-Complexity Pruning. The subtree with the largest cost complexity that is smaller than ccp_alpha will be chosen. By default, no pruning is performed.',
+            default=0.0,
+            forOptimizer=True,
+            min=0.0,
+            maxForOptimizer=0.1))
+
+lale.docstrings.set_docstrings(GradientBoostingClassifierImpl, GradientBoostingClassifier._schemas)
