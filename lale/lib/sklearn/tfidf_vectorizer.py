4e26d26f158552004c62b5584473fade85b917e3
kakate@us.ibm.com <kiran.kate@gmail.com>
2020-10-30 10:21:34 -0400

    Changing the tfidf input type to string/unicode.

--- lale/lib/sklearn/tfidf_vectorizer.py
+++ lale/lib/sklearn/tfidf_vectorizer.py
@@ -31,13 +31,13 @@ class TfidfVectorizerImpl:
 
     def fit(self, X, y=None):
         if isinstance(X, np.ndarray) or isinstance(X, pd.DataFrame):
-            X = X.squeeze()
+            X = X.squeeze().astype("U")
         self._wrapped_model.fit(X, y)
         return self
 
     def transform(self, X):
         if isinstance(X, np.ndarray) or isinstance(X, pd.DataFrame):
-            X = X.squeeze()
+            X = X.squeeze().astype("U")
         return self._wrapped_model.transform(X)
 
 
