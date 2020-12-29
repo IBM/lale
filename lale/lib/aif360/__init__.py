c648ccc535e824ac7b8171a479e2f9cd2a3c5bda
Martin Hirzel <hirzel@us.ibm.com>
2020-12-29 15:39:57 -0500

    Fleshed out module-level documentation for lale.lib.aif360. Added three more metrics. Improved warnings for misconfigured metrics.

--- lale/lib/aif360/__init__.py
+++ lale/lib/aif360/__init__.py
@@ -13,9 +13,74 @@
 # limitations under the License.
 
 """
-Scikit-learn compatible wrappers for most of the operators from AIF360_ along with schemas to enable hyperparameter tuning.
+Scikit-learn compatible wrappers for several operators and metrics from AIF360_ along with schemas to enable hyperparameter tuning.
 
 .. _AIF360: https://github.com/IBM/AIF360
+
+All operators and metrics in the Lale wrappers for AIF360 take two
+arguments, `favorable_labels` and `protected_attributes`, collectively
+referred to as *fairness info*. For example, the following code
+indicates that male values in the `personal_status` attribute belong
+to the priviliged group:
+
+.. code:: Python
+
+    fairness_info_sex = {
+        "favorable_labels": ["good"],
+        "protected_attributes": [
+            {
+                "feature": "personal_status",
+                "privileged_groups": [
+                    "male div/sep", "male mar/wid", "male single",
+                ],
+            },
+        ],
+    }
+
+Similarly, the following code indicates that values from 26 to 1000 in
+the `age` attribute belong to the privileged group:
+
+.. code:: Python
+
+    fairness_info_age = {
+        "favorable_labels": ["good"],
+        "protected_attributes": [
+            {"feature": "age", "privileged_groups": [[26, 1000]]},
+        ],
+    }
+
+AIF360 provides three kinds of fairness mitigators, illustrated in the
+following picture. *Preprocessing* mitigators transform the data
+before it gets to an estimator; *inprocessing* mitigators include
+their own estimator; and *postprocessing* mitigators transform
+predictions *after* those come back from an estimator.
+
+.. image:: ../../docs/img/fairness_patterns.png
+
+In the picture, italics indicate parameters of the pattern. For
+example, consider the following code:
+
+.. code:: Python
+
+    pipeline = LFR(
+        **fairness_info,
+        preprocessing=(
+            (Project(columns={"type": "string"}) >> OneHotEncoder(handle_unknown="ignore"))
+            & Project(columns={"type": "number"})
+        )
+        >> ConcatFeatures
+    ) >> LogisticRegression(max_iter=1000)
+
+In this example, the *mitigator* is `LFR` preprocessing, the
+*estimator* is `LogisticRegression`, and the *preprocessing* is a
+sub-pipeline that one-hot-encodes strings. If all features of the data
+are numerical, then the preprocessing can be omitted. Internally, the
+`LFR` higher-order operator uses two auxiliary operators, Redacting
+and ProtectedAttributesEncoder.  Redacting sets protected attributes
+to a constant to prevent them from directly influencing
+fairness-agnostic preprocessing or estimators. And the
+ProtectedAttributesEncoder encodes protected attributes and labels as
+zero or one to simplify the task for the mitigator.
 
 Preprocessing Operators:
 ========================
@@ -38,9 +103,12 @@ Postprocessing Operators:
 Metrics:
 ========
 * `accuracy_and_disparate_impact`_
+* `average_odds_difference`_
 * `disparate_impact`_
+* `equal_opportunity_difference`_
 * `r2_and_disparate_impact`_
 * `statistical_parity_difference`_
+* `theil_index`_
 
 Other Operators:
 ================
@@ -66,11 +134,14 @@ Other Functions:
 .. _`RejectOptionClassification`: lale.lib.aif360.reject_option_classification.html
 .. _`Reweighing`: lale.lib.aif360.reweighing.html
 .. _`accuracy_and_disparate_impact`: lale.lib.aif360.util.html#lale.lib.aif360.util.accuracy_and_disparate_impact
+.. _`average_odds_difference`: lale.lib.aif360.util.html#lale.lib.aif360.util.average_odds_difference
 .. _`dataset_fairness_info`: lale.lib.aif360.util.html#lale.lib.aif360.util.dataset_fairness_info
 .. _`dataset_to_pandas`: lale.lib.aif360.util.html#lale.lib.aif360.util.dataset_to_pandas
 .. _`disparate_impact`: lale.lib.aif360.util.html#lale.lib.aif360.util.disparate_impact
+.. _`equal_opportunity_difference`: lale.lib.aif360.util.html#lale.lib.aif360.util.equal_opportunity_difference
 .. _`r2_and_disparate_impact`: lale.lib.aif360.util.html#lale.lib.aif360.util.r2_and_disparate_impact
 .. _`statistical_parity_difference`: lale.lib.aif360.util.html#lale.lib.aif360.util.statistical_parity_difference
+.. _`theil_index`: lale.lib.aif360.util.html#lale.lib.aif360.util.theil_index
 """
 
 from .adversarial_debiasing import AdversarialDebiasing
@@ -88,11 +159,14 @@ from .reject_option_classification import RejectOptionClassification
 from .reweighing import Reweighing
 from .util import (
     accuracy_and_disparate_impact,
+    average_odds_difference,
     dataset_fairness_info,
     dataset_to_pandas,
     disparate_impact,
+    equal_opportunity_difference,
     fetch_adult_df,
     fetch_creditg_df,
     r2_and_disparate_impact,
     statistical_parity_difference,
+    theil_index,
 )
