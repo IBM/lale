beca0428584466ef96e1a51f7c13f6776ffce485
Vicky <vicky.vishal.sahu@gmail.com>
2021-03-17 19:27:14 +0100

    changes

--- lale/lib/sklearn/dummy_classifier.py
+++ lale/lib/sklearn/dummy_classifier.py
@@ -18,8 +18,6 @@ import sklearn.dummy
 
 import lale.docstrings
 import lale.operators
-from lale.schemas import AnyOf, Enum, Float, Null
-
 
 _hyperparams_schema = {
     "allOf": [
@@ -28,7 +26,49 @@ _hyperparams_schema = {
             "type": "object",
             "relevantToOptimizer": [],
             "additionalProperties": False,
-        }
+            "required": [
+                "strategy",
+                "random_state",
+            ],
+            "properties": {
+                "strategy": {
+                    "description": """Strategy to use to generate predictions.
+- “stratified”: generates predictions by respecting the training set’s class distribution.
+- “most_frequent”: always predicts the most frequent label in the training set.
+- “prior”: always predicts the class that maximizes the class prior (like “most_frequent”) and predict_proba returns the class prior.
+- “uniform”: generates predictions uniformly at random.
+- “constant”: always predicts a constant label that is provided by the user. This is useful for metrics that evaluate a non-majority class""",
+                    "enum": [
+                        "stratified",
+                        "most_frequent",
+                        "prior",
+                        "uniform",
+                        "constant",
+                    ],
+                    "default": "prior",
+                },
+                "random_state": {
+                    "description": "Seed of pseudo-random number generator for shuffling data when solver == ‘sag’, ‘saga’ or ‘liblinear’.",
+                    "anyOf": [
+                        {
+                            "description": "RandomState used by np.random",
+                            "enum": [None],
+                        },
+                        {
+                            "description": "Use the provided random state, only affecting other users of that same random state instance.",
+                            "laleType": "numpy.random.RandomState",
+                        },
+                        {"description": "Explicit seed.", "type": "integer"},
+                    ],
+                    "default": None,
+                },
+                "constant": {
+                    "description": "The explicit constant as predicted by the “constant” strategy. This parameter is useful only for the “constant” strategy.",
+                    "side constraint": "",
+                    "type": ["int", "str"],
+                },
+            },
+        },
     ]
 }
 
@@ -72,9 +112,11 @@ _output_predict_schema = {
 
 _combined_schemas = {
     "$schema": "http://json-schema.org/draft-04/schema#",
-    "description": "Baseline classifier always predicts the majority class.",
-    "documentation_url": "https://lale.readthedocs.io/en/latest/modules/lale.lib.lale.baseline_classifier.html",
-    "import_from": "lale.lib.lale",
+    "description": """`Dummy classifier`_ classifier that makes predictions using simple rules.
+
+.. _`Dummy classifier`: https://scikit-learn.org/stable/modules/generated/sklearn.dummy.DummyClassifier.html
+""",
+    "import_from": "sklearn.dummy",
     "type": "object",
     "tags": {"pre": [], "op": ["estimator", "classifier"], "post": []},
     "properties": {
