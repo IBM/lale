9347707063f584f638bd5f46114ea2879ad1585f
kakate@us.ibm.com <kiran.kate@gmail.com>
2021-04-13 09:54:50 -0400

    Pinning the xgboost version.

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
     install_requires=install_requires,
     extras_require={
         "full": [
-            "xgboost",
+            "xgboost==1.3.3",
             "lightgbm",
             "snapml>=1.7.0rc3",
             "liac-arff>=2.4.0",
