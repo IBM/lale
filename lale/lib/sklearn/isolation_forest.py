c4f1d159df61e26bc041eea70a41e83503bfbf6b
kakate@us.ibm.com <kiran.kate@gmail.com>
2021-06-08 12:16:36 -0400

    Added max_samples ranges for IsolationForest.

--- lale/lib/sklearn/isolation_forest.py
+++ lale/lib/sklearn/isolation_forest.py
@@ -74,6 +74,8 @@ produce shorter path lengths for particular samples, they are highly likely to b
                             "minimum": 0.0,
                             "exclusiveMinimum": True,
                             "maximum": 1.0,
+                            "minimumForOptimizer": 0.0,
+                            "maximumForOptimizer": 1.0,
                         },
                         {
                             "description": "Draw max_samples=min(256, n_samples) samples.",
