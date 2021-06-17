57255d6889aa7324210033f692501ee0f1131af0
kakate@us.ibm.com <kiran.kate@gmail.com>
2021-06-17 15:29:14 -0400

    Updated `sklearn_version_family` for CatEncoder as well.

--- lale/lib/autoai_libs/cat_encoder.py
+++ lale/lib/autoai_libs/cat_encoder.py
@@ -151,7 +151,7 @@ In the inverse transform, an unknown category will be denoted as None.""",
                 },
                 "sklearn_version_family": {
                     "description": "The sklearn version for backward compatibiity with versions 019 and 020dev. Currently unused.",
-                    "enum": ["20", "23", None],
+                    "enum": ["20", "21", "22", "23", "24", None],
                     "default": None,
                 },
                 "activate_flag": {
