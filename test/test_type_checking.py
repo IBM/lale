c70a02a0c9ee0eb961db6205f87298d97d19b823
shinnar <shinnar@users.noreply.github.com>
2021-06-21 22:43:33 -0400

    Suggest hyperparam fixes when hyperparams don't validate against the schema (#741)
    * Suggest hyperparam fixes when hyperparams don't validate against the schema
    Includes fix suggestions for removing unknown hyperparameter keys

--- test/test_type_checking.py
+++ test/test_type_checking.py
@@ -683,6 +683,28 @@ class TestErrorMessages(unittest.TestCase):
                 summary,
                 "Invalid configuration for LogisticRegression(C=-1) due to invalid value C=-1.",
             )
+            fix1 = cm.exception.message.split("\n")[2]
+            self.assertRegex(fix1, "C=1.0")
+
+    def test_fixes2(self):
+        with EnableSchemaValidation():
+            with self.assertRaises(jsonschema.ValidationError) as cm:
+                LogisticRegression(
+                    penalty="l1",
+                    solver="liblinear",
+                    multi_class="multinomial",
+                    dual=True,
+                )
+            summary = cm.exception.message.split("\n")[0]
+            self.assertRegex(
+                summary,
+                "Invalid configuration for LogisticRegression(.*) due to constraint",
+            )
+            fix1 = cm.exception.message.split("\n")[2]
+            fix2 = cm.exception.message.split("\n")[3]
+            # we don't care what order they are in
+            self.assertRegex(fix1 + fix2, "penalty='l2', multi_class='auto'")
+            self.assertRegex(fix1 + fix2, "multi_class='auto', dual=False")
 
     def test_wrong_cat(self):
         with EnableSchemaValidation():
@@ -703,6 +725,8 @@ class TestErrorMessages(unittest.TestCase):
                 summary,
                 "Invalid configuration for LogisticRegression(activation='relu') due to argument 'activation' was unexpected.",
             )
+            fix1 = cm.exception.message.split("\n")[1]
+            self.assertRegex(fix1, "remove unknown key 'activation'")
 
     def test_constraint(self):
         with EnableSchemaValidation():
@@ -713,6 +737,52 @@ class TestErrorMessages(unittest.TestCase):
                 summary,
                 "Invalid configuration for LogisticRegression(solver='sag', penalty='l1') due to constraint the newton-cg, sag, and lbfgs solvers support only l2 or no penalties.",
             )
+            fix1 = cm.exception.message.split("\n")[2]
+            self.assertRegex(fix1, "Set penalty='l2'")
+
+    def test_unknown_arg_and_constraint2(self):
+        with EnableSchemaValidation():
+            with EnableSchemaValidation():
+                with self.assertRaises(jsonschema.ValidationError) as cm:
+                    LogisticRegression(
+                        activation="relu",
+                        penalty="l1",
+                        solver="liblinear",
+                        multi_class="multinomial",
+                        dual=True,
+                    )
+                summary = cm.exception.message.split("\n")[0]
+
+                self.assertRegex(
+                    summary,
+                    "Invalid configuration for LogisticRegression.*due to argument 'activation' was unexpected.",
+                )
+
+                fix1 = cm.exception.message.split("\n")[2]
+                fix2 = cm.exception.message.split("\n")[3]
+                # we don't care what order they are in
+                self.assertRegex(
+                    fix1 + fix2, "Remove unknown key 'activation'.*Set.*penalty='l2'"
+                )
+                self.assertRegex(
+                    fix1 + fix2, "Remove unknown key 'activation'.*Set.*dual=False"
+                )
+                self.assertRegex(
+                    fix1 + fix2,
+                    "Remove unknown key 'activation'.*Set.*multi_class='auto'",
+                )
+
+    def test_unknown_arg_and_constraint(self):
+        with EnableSchemaValidation():
+            with self.assertRaises(jsonschema.ValidationError) as cm:
+                LogisticRegression(activation="relu", solver="sag", penalty="l1")
+            summary = cm.exception.message.split("\n")[0]
+            self.assertRegex(
+                summary,
+                "Invalid configuration for LogisticRegression.*due to argument 'activation' was unexpected.",
+            )
+            fix1 = cm.exception.message.split("\n")[2]
+            self.assertRegex(fix1, "Remove unknown key 'activation'.*Set penalty='l2'")
 
 
 class TestSchemaValidation(unittest.TestCase):
