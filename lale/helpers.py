5ccd14d43d813cd1463a01836910d12bf4c8e92d
Panagiotis Hadjidoukas (Chatzidoukas) <phadjido@gmail.com>
2021-08-18 20:53:06 +0200

    Update helpers.py

--- lale/helpers.py
+++ lale/helpers.py
@@ -826,7 +826,8 @@ def create_data_loader(X, y=None, batch_size=1, num_workers=0):
         batch_size=batch_size,
         collate_fn=collate_fn,
         num_workers=num_workers,
-        worker_init_fn=worker_init_fn)
+        worker_init_fn=worker_init_fn,
+    )
 
 
 def write_batch_output_to_file(
