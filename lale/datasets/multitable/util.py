6462f69e2ec0d742909e0aa9d61effcb3a11b10d
kakate@us.ibm.com <kiran.kate@gmail.com>
2021-09-10 14:45:01 -0400

    Fixed pre-commit errors.

--- lale/datasets/multitable/util.py
+++ lale/datasets/multitable/util.py
@@ -12,18 +12,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from numpy import random
 from typing import Tuple
+
+from numpy import random
 
 try:
     from pyspark.sql import SparkSession
+
     spark_installed = True
 except ImportError:
     spark_installed = False
 
 
-from lale.datasets.data_schemas import get_table_name, add_table_name
-from lale.helpers import _is_spark_df, _is_pandas_df
+from lale.datasets.data_schemas import add_table_name, get_table_name
+from lale.helpers import _is_pandas_df, _is_spark_df
+
 
 def multitable_train_test_split(
     dataset,
@@ -88,14 +91,18 @@ def multitable_train_test_split(
             index_of_main_table = i
     if main_table_df is None:
         table_names = [get_table_name(df) for df in dataset]
-        raise ValueError(f"Could not find {main_table_name} in the given dataset, the table names are {table_names}")
+        raise ValueError(
+            f"Could not find {main_table_name} in the given dataset, the table names are {table_names}"
+        )
     if _is_pandas_df(main_table_df):
         num_rows = len(main_table_df)
     elif _is_spark_df(main_table_df):
-        #main_table_df = main_table_df.toPandas()
+        # main_table_df = main_table_df.toPandas()
         num_rows = main_table_df.count()
     else:
-        raise ValueError("multitable_train_test_split can only work with a list of Pandas or Spark dataframes.")
+        raise ValueError(
+            "multitable_train_test_split can only work with a list of Pandas or Spark dataframes."
+        )
     if test_size > 0 and test_size < 1:
         num_test_rows = int(num_rows * test_size)
     else:
@@ -109,11 +116,19 @@ def multitable_train_test_split(
         train_main_df = main_table_df.iloc[train_indices]
         test_main_df = main_table_df.iloc[test_indices]
     elif _is_spark_df(main_table_df):
-        spark_session = SparkSession.builder.appName("multitable_train_test_split").getOrCreate()
-        train_main_df = spark_session.createDataFrame(data=main_table_df.toPandas().iloc[train_indices])
-        test_main_df = spark_session.createDataFrame(data=main_table_df.toPandas().iloc[test_indices])
+        spark_session = SparkSession.builder.appName(
+            "multitable_train_test_split"
+        ).getOrCreate()
+        train_main_df = spark_session.createDataFrame(
+            data=main_table_df.toPandas().iloc[train_indices]
+        )
+        test_main_df = spark_session.createDataFrame(
+            data=main_table_df.toPandas().iloc[test_indices]
+        )
     else:
-        raise ValueError("multitable_train_test_split can only work with a list of Pandas or Spark dataframes.")
+        raise ValueError(
+            "multitable_train_test_split can only work with a list of Pandas or Spark dataframes."
+        )
 
     train_main_df = add_table_name(train_main_df, main_table_name)
     test_main_df = add_table_name(test_main_df, main_table_name)
