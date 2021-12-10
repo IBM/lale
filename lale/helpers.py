9875338a84e7e53e4919b793a66289f6dc064613
kiran-kate <40038037+kiran-kate@users.noreply.github.com>
2021-12-10 16:45:37 -0500

    Trying a different way check if pyspark exists. (#913)
    * Trying a different way check if pyspark exists.
    
    * Fixed the import of importlib.util.find_spec.

--- lale/helpers.py
+++ lale/helpers.py
@@ -51,13 +51,12 @@ try:
 except ImportError:
     torch_installed = False
 
-try:
+from importlib import util
+
+spark_loader = util.find_spec("pyspark")
+spark_installed = spark_loader is not None
+if spark_installed:
     from pyspark.sql.dataframe import DataFrame as spark_df
-
-    spark_installed = True
-
-except ImportError:
-    spark_installed = False
 
 logger = logging.getLogger(__name__)
 
