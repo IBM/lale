d57cec1e0395364386d3dc2df84c42c47716f764
Louis Mandel <lmandel@us.ibm.com>
2021-12-15 10:55:22 -0500

    Remove `fit_transform`

--- lale/lib/rasl/min_max_scaler.py
+++ lale/lib/rasl/min_max_scaler.py
@@ -70,9 +70,6 @@ class _MinMaxScalerImpl:
     def transform(self, X):
         return self.transformer.transform(X)
 
-    def fit_transform(self, X, y=None):
-        scaler = self.fit(X, y)
-        return scaler.transform(X)
 
 _combined_schemas = {
     "$schema": "http://json-schema.org/draft-04/schema#",
