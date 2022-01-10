bd7e46569d91d8ebb38852670d71f7bc3474e2dc
kakate@us.ibm.com <kiran.kate@gmail.com>
2022-01-10 15:25:44 -0500

    Install autoai-libs from test pypi.

--- lale/lib/sklearn/one_hot_encoder.py
+++ lale/lib/sklearn/one_hot_encoder.py
@@ -187,8 +187,9 @@ class _OneHotEncoderImpl:
 OneHotEncoder = lale.operators.make_operator(_OneHotEncoderImpl, _combined_schemas)
 
 sklearn_version_str = getattr(sklearn, "__version__")
+sklearn_version = version.parse(sklearn_version_str)
 
-if sklearn_version_str >= version.Version("0.21"):
+if sklearn_version >= version.Version("0.21"):
     # new: https://scikit-learn.org/0.21/modules/generated/sklearn.preprocessing.OneHotEncoder.html
     OneHotEncoder = OneHotEncoder.customize_schema(
         drop={
@@ -220,7 +221,7 @@ if sklearn_version_str >= version.Version("0.21"):
         set_as_available=True,
     )
 
-if sklearn_version_str >= version.Version("0.23"):
+if sklearn_version >= version.Version("0.23"):
     # new: https://scikit-learn.org/0.23/modules/generated/sklearn.preprocessing.OneHotEncoder.html
     OneHotEncoder = OneHotEncoder.customize_schema(
         drop={
