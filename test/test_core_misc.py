IBM/lale

commit 9d8a535846ef6b6f15d66ffee9f4b9766ea2c911

get_param_ranges treats booleans as enums, and combines with other enums (fixes #837)

lale/operators.py
@@ -1812,25 +1812,30 @@ class IndividualOp(Operator):
                         if "enum" in s:
                             if ("forOptimizer" not in s) or s["forOptimizer"]:
                                 enums.append(s)
+                        elif s.get("type", None) == "boolean":
+                            if ("forOptimizer" not in s) or s["forOptimizer"]:
+                                bool_s = {"enum": [False, True]}
+                                d = s.get("default", None)
+                                if d is not None:
+                                    bool_s["default"] = d
+
+                                enums.append(bool_s)
                     if len(enums) == 1:
                         return enums[0]
                     elif enums:
                         # combine them, and see if there is an anyOf default that we want to use as well
                         vals = [item for s in enums for item in s["enum"]]
-                        d = None
-                        sch_d = schema.get("default", None)
-                        if sch_d and sch_d in vals:
-                            d = sch_d
-                        else:
-                            for s in enums:
-                                if "default" in s:
-                                    d = s["default"]
-                                    break
                         new_s = {
                             "enum": vals,
                         }
-                        if d is not None:
-                            new_s["default"] = d
+
+                        if "default" in schema and schema["default"] in vals:
+                            new_s["default"] = schema["default"]
+                        else:
+                            for s in enums:
+                                if "default" in s:
+                                    new_s["default"] = s["default"]
+                                    break
                         return new_s
 
                 return schema["anyOf"][0]
@@ -1871,8 +1876,6 @@ class IndividualOp(Operator):
                 default = schema["default"]
                 non_default = [v for v in schema["enum"] if v != default]
                 return [*non_default, default]
-            elif schema["type"] == "boolean":
-                return (False, True, schema["default"])
             else:
 
                 def get(schema, key):

test/test_core_misc.py
@@ -955,6 +955,31 @@ class TestHyperparamRanges(unittest.TestCase):
         self.maxDiff = None
         self.assertEqual(ranges, expected_ranges)
 
+    def test_bool_enum(self):
+        from lale.lib.sklearn import SVR
+        from lale.schemas import AnyOf, Bool, Null
+
+        SVR = SVR.customize_schema(
+            shrinking=AnyOf(
+                types=[Bool(), Null()],
+                default=None,
+                desc="Whether to use the shrinking heuristic.",
+            )
+        )
+
+        ranges, dists = SVR.get_param_ranges()
+        expected_ranges = {
+            "kernel": ["poly", "rbf", "sigmoid", "linear"],
+            "degree": (2, 5, 3),
+            "gamma": (3.0517578125e-05, 8, None),
+            "tol": (0.0, 0.01, 0.001),
+            "C": (0.03125, 32768, 1.0),
+            "shrinking": [False, True, None],
+        }
+
+        self.maxDiff = None
+        self.assertEqual(ranges, expected_ranges)
+
 
 class TestScoreIndividualOp(unittest.TestCase):
     def setUp(self):

commit a9390addca5bb41573c2cfbfb2e02c4492d583e3

Fix regression for boolean schemas in get_param_ranges accidentally introduced in 9d8a535846ef6b6f15d66ffee9f4b9766ea2c911

lale/operators.py
@@ -1969,6 +1969,8 @@ class IndividualOp(Operator):
                 default = schema["default"]
                 non_default = [v for v in schema["enum"] if v != default]
                 return [*non_default, default]
+            elif schema["type"] == "boolean":
+                return (False, True, schema["default"])
             else:
 
                 def get(schema, key):

test/test_core_misc.py
@@ -955,6 +955,25 @@ class TestHyperparamRanges(unittest.TestCase):
         self.maxDiff = None
         self.assertEqual(ranges, expected_ranges)
 
+    def test_logisticregression(self):
+        from lale.lib.sklearn import LogisticRegression
+
+        ranges, dists = LogisticRegression.get_param_ranges()
+        expected_ranges = {
+            "solver": ["newton-cg", "liblinear", "sag", "saga", "lbfgs"],
+            "dual": (False, True, False),
+            "tol": (1e-08, 0.01, 0.0001),
+            "fit_intercept": (False, True, True),
+            "intercept_scaling": (0.0, 1.0, 1.0),
+            "max_iter": (10, 1000, 100),
+            "multi_class": ["ovr", "multinomial", "auto"],
+        }
+        expected_dists = {"solver": (0, 4, 4), "multi_class": (0, 2, 2)}
+
+        self.maxDiff = None
+        self.assertEqual(ranges, expected_ranges)
+        self.assertEqual(dists, expected_dists)
+
     def test_bool_enum(self):
         from lale.lib.sklearn import SVR
         from lale.schemas import AnyOf, Bool, Null