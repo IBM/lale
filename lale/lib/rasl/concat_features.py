25dcbcb0432be8f9d55d15e5f23472de4f1ed455
Louis Mandel <lmandel@us.ibm.com>
2022-02-25 15:00:54 -0500

    Fix schema

--- lale/lib/rasl/concat_features.py
+++ lale/lib/rasl/concat_features.py
@@ -194,6 +194,7 @@ _hyperparams_schema = {
             "type": "object",
             "additionalProperties": False,
             "relevantToOptimizer": [],
+            "properties": {},
         }
     ]
 }
