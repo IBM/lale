345380b91e6e70a882a2059c80d9de953a5fc59b
Louis Mandel <lmandel@us.ibm.com>
2022-03-16 13:57:01 -0400

    Ensure same error message

--- lale/type_checking.py
+++ lale/type_checking.py
@@ -114,8 +114,13 @@ def always_validate_schema(value, schema: JSON_TYPE, subsample_array: bool = Tru
         The value was invalid for the schema.
     """
     json_value = lale.helpers.data_to_json(value, subsample_array)
-    validator = _lale_validator(lale.helpers.data_to_json(schema, False))
-    validator.validate(json_value)
+    try:
+        validator = _lale_validator(lale.helpers.data_to_json(schema, False))
+        validator.validate(json_value)
+    except Exception:
+        jsonschema.validate(
+            json_value, lale.helpers.data_to_json(schema, False), _lale_validator
+        )
 
 
 def validate_schema_directly(value, schema: JSON_TYPE, subsample_array: bool = True):
