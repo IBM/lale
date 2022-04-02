09d94d7bd0a1cde06df9b6c014c75f3b99909c84
Martin Hirzel <hirzel@gmail.com>
2022-04-02 08:01:56 -0400

    Constraining scipy version. (#1068)

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright 2019 IBM Corporation
+# Copyright 2019-2022 IBM Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -50,7 +50,7 @@ else:
         "jsonschema",
         "jsonsubschema>=0.0.6",
         "scikit-learn",
-        "scipy",
+        "scipy==1.5.2",
         "pandas",
         "decorator",
         "astunparse",
