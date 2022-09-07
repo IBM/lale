d856a94b32b3085be07455341db73808dab6c208
Avi Shinnar <shinnar@us.ibm.com>
2022-09-07 17:05:18 -0400

    upper and lower bound sklearn version
    Signed-off-by: Avi Shinnar <shinnar@us.ibm.com>

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ else:
         "hyperopt>=0.2,<=0.2.5",
         "jsonschema",
         "jsonsubschema>=0.0.6",
-        "scikit-learn",
+        "scikit-learn>=1.0.0,<=1.1.1",
         "scipy<1.9",
         "pandas",
         "decorator",
