abb587ff2cb617c4fc949d854964c31e6348fb0a
TrellixVulnTeam <charles.mcfarland@trellix.com>
2023-01-05 19:53:12 +0000

    Adding tarfile member sanitization to extractall()

--- lale/datasets/movie_review.py
+++ lale/datasets/movie_review.py
@@ -37,7 +37,26 @@ def load_movie_review():
     X = []
     y = []
     with tarfile.open(data_file_path) as data_file:
-        data_file.extractall(path=download_data_dir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner) 
+            
+        
+        safe_extract(data_file, path=download_data_dir)
 
     with open(
         os.path.join(download_data_dir, "rt-polaritydata", "rt-polarity.neg"), "rb"
