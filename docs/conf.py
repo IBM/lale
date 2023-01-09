06affa4897c991c8f66df4ff4f725d9818a54509
Avi Shinnar <shinnar@us.ibm.com>
2023-01-09 10:43:57 -0500

    Fix settings in sphinx conf
    Signed-off-by: Avi Shinnar <shinnar@us.ibm.com>

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@
 import builtins
 import os
 import sys
-from typing import Dict
+from typing import Dict, List
 
 from lale.settings import set_disable_hyperparams_schema_validation
 
@@ -115,7 +115,7 @@ master_doc = "index"
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -143,7 +143,8 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
+html_static_path: List[str] = []
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
