dab10418c509a332364daaaa8e1bdbdc956f40dc
Avi Shinnar <shinnar@us.ibm.com>
2024-07-30 13:45:52 -0400

    docs: Remove numpy upper bound from docs build and mock mystic
    Signed-off-by: Avi Shinnar <shinnar@us.ibm.com>

--- docs/conf.py
+++ docs/conf.py
@@ -96,10 +96,11 @@ autodoc_mock_imports = [
     "aif360",
     "ConfigSpace",
     "fairlearn",
+    "mystic",
+    "numba",
     "pytorch",
     "tensorflow",
     "torch",
-    "numba",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
