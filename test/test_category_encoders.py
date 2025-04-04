7c7b6a1e1a4ad2504d18cd93439da53c5f322569
Avi Shinnar <shinnar@us.ibm.com>
2025-04-04 06:15:44 -0400

    Disable category encoder tests on unsupported dependency version combinations
    Signed-off-by: Avi Shinnar <shinnar@us.ibm.com>

--- test/test_category_encoders.py
+++ test/test_category_encoders.py
@@ -21,6 +21,25 @@ from lale.lib.rasl import ConcatFeatures, Project
 from lale.lib.sklearn import LogisticRegression
 
 
+def is_cat_ok() -> bool:
+    try:
+        import category_encoders
+        from packaging import version
+
+        from lale.operators import sklearn_version
+
+        catenc_version = version.parse(getattr(category_encoders, "__version__"))
+
+        if sklearn_version >= version.Version(
+            "1.6"
+        ) and catenc_version < version.Version("2.8"):
+            return False
+        return True
+
+    except ImportError:
+        return False
+
+
 class TestCategoryEncoders(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
@@ -31,6 +50,10 @@ class TestCategoryEncoders(unittest.TestCase):
             astype="pandas",
         )
 
+    @unittest.skipUnless(
+        is_cat_ok(),
+        "Unsupported combination of category encoder and scikit-learn.  Note that python 3.9 does not work",
+    )
     def test_hashing_encoder(self):
         (train_X, train_y), (test_X, _test_y) = self.creditg
         cat_prep = Project(columns={"type": "string"}) >> HashingEncoder()
@@ -41,6 +64,10 @@ class TestCategoryEncoders(unittest.TestCase):
         trained = trainable.fit(train_X, train_y)
         _ = trained.predict(test_X)
 
+    @unittest.skipUnless(
+        is_cat_ok(),
+        "Unsupported combination of category encoder and scikit-learn.  Note that python 3.9 does not work",
+    )
     def test_target_encoder(self):
         (train_X, train_y), (test_X, _test_y) = self.creditg
         cat_prep = Project(columns={"type": "string"}) >> TargetEncoder()
